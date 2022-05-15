ce92e3b6704fc5015a3abb373ddf1e148f8bd4e0
carla-cn <75775973+carla-cn@users.noreply.github.com>
2022-05-16 07:55:34 +0800

    修改一个gug

--- src/redux/countAction.js
+++ src/redux/countAction.js
@@ -10,8 +10,8 @@ export const createDecrementAction = data => ({ type: DECREMENT, data })
 // 异步action，就是指action的值为函数，异步action中一般会调用同步action
 // 注意：异步action不是必须要用的
 export const createIncrementAsyncAction = (data, time) => {
-	return () => {
-		setTimeout(dispatch => {
+	return (dispatch) => {
+		setTimeout(() => {
 			dispatch(createIncrementAction(data))
 		}, time)
 	}
