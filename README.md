afbdf028066842b1b283b1e9d91be3f28e3e64fe
zhouling <zhouling@kezaihui.com>
2022-05-16 22:27:51 +0800

    30_react-redux_开发者工具

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-# 一、纯函数
+# 一、求和 redux-react 开发者工具使用
 
-1. 一类特别的函数：只要是同样的输入(实参)，必定得到同样的输出(返回)
-2. 必须遵守以下一些约束：
+1. `yarn add redux-devtools-extension`
 
-   - 不得改写参数数据
-   - 不会产生任何副作用，例如网络请求，输入和输出设备
-   - 不能调用 Date.now()或者 Math.random()等不纯的方法
-
-3. redux 的 reducer 函数必须是一个纯函数
+2. store 中进行配置
+   ```javascript
+   import { composeWithDevTools } from 'redux-devtools-extension'
+   export default createStore(allReducer, composeWithDevTools(applyMiddleware(thunk)))
+   ```
--- src/redux/store.js
+++ src/redux/store.js
@@ -10,8 +10,10 @@ import countReducer from './reducers/count'
 import personReducer from './reducers/person'
 // 引入redux-thunk，用于支持异步action
 import thunk from 'redux-thunk'
+// 引入 redux-devtools-extension
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 const allReducer = combineReducers({ count: countReducer, persons: personReducer })
 
 // 暴露store
-export default createStore(allReducer, applyMiddleware(thunk))
+export default createStore(allReducer, composeWithDevTools(applyMiddleware(thunk)))
